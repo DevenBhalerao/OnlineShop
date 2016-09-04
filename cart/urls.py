fc198afed64f1aaac20df56a8f31b57366ca4ee9
DevenBhalerao <devenbhalerao@gmail.com>
2016-09-05 01:09:40 +0530

    minor url fix

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.cart_detail, name='cart_detail'),
     url(r'^add/(?P<product_id>\d+)/$', views.add_cart, name='add_cart'),
-    url(r'^add/(?P<product_id>\d+)/$', views.cart_remove, name='remove_cart'),
+    url(r'^remove/(?P<product_id>\d+)/$', views.cart_remove,
+        name='cart_remove'),
 ]
