dfd8673f377a523990ab3304542e8d5cdd1a1eeb
DevenBhalerao <devenbhalerao@gmail.com>
2016-09-04 23:46:11 +0530

    added cart form

--- shop1/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class Shop1Config(AppConfig):
-    name = 'shop1'
--- shop1/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def product_list(request):
-    return render(request,
-                  'shop1/list.html',
-                  {})
